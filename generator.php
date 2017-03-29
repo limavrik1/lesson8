b9a2cdf3987631329900b36a93a1bb7096d290e0
Andrius Mikialionis <limavrik@gmail.com>
2017-03-29 17:27:35 +0300

    Add. Check for GET params

--- generator.php
+++ generator.php
@@ -5,4 +5,9 @@ if (!hasRole('auth-user') && !hasRole('administrator')) {
     http_response_code(403);
     die('Доступ запрещен');
 }
-echo hashPassword($_GET['login'],$_GET['password']);
+
+if (isset($_GET['login']) && isset($_GET['password'])) {
+    echo hashPassword($_GET['login'], $_GET['password']);
+} else {
+    echo 'Передайте GET параметры login и password';
+}
